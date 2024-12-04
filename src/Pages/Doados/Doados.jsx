e8995ff58c72cbcd5064c054719fe7a9ccf45b04
unknown <wlssilvacontato@gmail.com>
2024-12-04 20:40:41 -0300

    update project

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,31 @@
-
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
+import hpCameraSecret from '../../assets/hp_CameraSecret.png'
 
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxcard}>
+        
+                <article>
+                    <img src={livro} alt="imagem de um livro que se chama o protagonista" />
+                    
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                    
+                </article>
+                <article>
+                    <img src={hpCameraSecret} alt="imagem de um livro que se chama harry potter e a camera secreta" />
+                    
+                    <h3>Harry Potter e a câmera secreta</h3>
+                    <p>J. K. Rowling</p>
+                    <p>Aventura</p>
+                    
+                </article>
+            </section>
         </section>
     )
 }
